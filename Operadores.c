d6b5e122b686c9998da4561ed54768f29d7308e7
BetoSalas2015 <roberto.salazar@upaep.mx>
2020-06-11 19:58:40 -0500

    Codigo Sesiones 9 a 12 del 10-11/06/20

--- Operadores.c
+++ Operadores.c
@@ -1,6 +1,21 @@
 #include "defs.h"
 #include <stdlib.h>
 
+void Bases()
+{
+		a = 179;
+	printf("179 Base 10 es %o  base 8\n", a);
+	printf("179 Base 10 es %x  base 16\n", a);
+
+	b = 0263;
+	printf("263 Base 8 es %d  base 10\n", b);
+	printf("263 Base 8 es %x  base 16\n", b);
+
+	c = 0xb3;
+	printf("B3 Base 16 es %d  base 10\n", c);
+	printf("B3 Base 16 es %o  base 8\n", c);
+}
+
 void Primarios()
 {
 	// Operadores Primaros
@@ -33,6 +48,57 @@ int main(void)
 	PR(d, !a); NL;
 	PR(d, !!a); NL;
 	PR(d, !!!!!!a); NL;
+
+	// Operador mas/menos unario
+	a = -3; 
+	b = +3;
+	PRINT2(d, a, b);
+	PRINT1(+d,b);
+
+	// Complemento a 1 (~)
+	a = 20;
+	PRINT2(d, a, ~a);
+
+	// Operadores incremento/decremento (++/--)
+	a = 10;
+	a++;
+	PRINT1(d, a);
+	++a;
+	PRINT1(d, a);
+	a--;
+	PRINT1(d, a);
+	--a;
+	PRINT1(d, a);
+	a = 10;
+	PRINT1(d, a++);
+	a = 10;
+	PRINT1(d, ++a);
+	a = 10;
+	PRINT1(d,a+++b);
+	PRINT2(d, a, b);
+	a = 10; b = 5;
+	PRINT1(d,a++ + ++b);
+	PRINT2(d, a, b);
+	a = 10; b = 5;
+	PRINT1(d,++a + ++b);
+	PRINT2(d, a, b);
+	a = 5; b = 8; c = 0;
+	d = a++ + b-- + !c++;
+	PRINT1(d, d);
+	PRINT4(d, a, b, c, d);
+	
+	// Operador sizeof (sizeof)
+	printf("El tipo char ocupa %d bytes.\n", sizeof(char) );
+	printf("El tipo int ocupa %d bytes.\n", sizeof(int) );
+	printf("El tipo float ocupa %d bytes.\n", sizeof(float) );
+	printf("El tipo double ocupa %d bytes.\n", sizeof(double) );
+	printf("El variable f ocupa %d bytes.\n", sizeof(f) );
+	printf("El variable ptrJuanito ocupa %d bytes.\n", sizeof(ptrJuanito) );
+	printf("El tipo struct Persona ocupa %d bytes.\n", sizeof(struct Persona) );
+	printf("El arreglo Arreglo ocupa %d bytes.\n", sizeof(Arreglo) );
+
+
+
 
 	system("pause");
 }
--- defs.h
+++ defs.h
@@ -4,10 +4,10 @@
 #define NL putchar('\n')   
 
 #define PRINT1(f, x1) PR(f,x1), NL
-#define PRINT2(f,x1,x2) PR(f, x1) PRINT1(f,x2)
-#define PRINT3(f,x1,x2,x3) PR(f, x1) PRINT2(f,x2, x3)
-#define PRINT4(f,x1,x2,x3,x4) PR(f, x1) PRINT3(f,x2,x3,x4)
-#define PRINT5(f,x1,x2,x3,x4,x5) PR(f, x1) PRINT4(f,x2,x3,x4,x5)
+#define PRINT2(f,x1,x2) PR(f, x1), PRINT1(f,x2)
+#define PRINT3(f,x1,x2,x3) PR(f, x1), PRINT2(f,x2, x3)
+#define PRINT4(f,x1,x2,x3,x4) PR(f, x1), PRINT3(f,x2,x3,x4)
+#define PRINT5(f,x1,x2,x3,x4,x5) PR(f, x1), PRINT4(f,x2,x3,x4,x5)
 
 // Declaraciones Globales
 
@@ -19,6 +19,6 @@ struct Persona
 	int edad;
 } Juanito, *ptrJuanito;
 
-int a, b, c;
-float c, d, e;
+int a, b, c, d;
+float  e, f, g;
 
